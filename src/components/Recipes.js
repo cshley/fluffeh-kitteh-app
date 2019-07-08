cdb21c31d934ec0f5c38da5b51c67953c72cdbc0
Chloe Ashley <chloeashley193@gmail.com>
2019-07-08 16:44:23 +0100

    forgot to push (lots of changes)
    fixed classNames

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
 const Recipes = props => (
-    <div>
-  { props.recipes.map((recipe) => {
-       return (
-        <div key={recipe.recipe_id}> 
-        <img src={recipe.image_url} alt={recipe.title} />
-        <p> { recipe.title }</p>
+    <div className="container">
+        <div className="row">
+        { props.recipes.map((recipe) => {
+            return (
+                <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
+                    <div className="recipe__box">
+                        
+                        <img 
+                        className="recipe__box-img"
+                        src={recipe.image_url}
+                        alt={recipe.title}/>
+
+                        <div className="recipe__text">
+                            <h5>{ recipe.title }</h5>
+                            <p  className="recipes__subtitles">Publisher: <span>
+                                { recipe.publisher }
+                                </span> </p>
+                                <button className="recipe__button">View Recipe</button>
+                        </div>
+
+                    </div>
+                </div>
+           
+            ); 
+        })}            
         </div>
-       ) 
-     }) }
     </div>
 );
 
