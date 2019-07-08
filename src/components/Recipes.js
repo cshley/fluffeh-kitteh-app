8a55f8f9b4e7a62e85ead56a8544a8bb8d3f4665
Chloe Ashley <chloeashley193@gmail.com>
2019-07-08 17:04:28 +0100

    title length fix

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,7 +6,7 @@ const Recipes = props => (
         { props.recipes.map((recipe) => {
             return (
                 <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
-                    <div className="recipes__box">
+                    <div className="recipe__box">
                         
                         <img 
                         className="recipe__box-img"
@@ -14,7 +14,8 @@ const Recipes = props => (
                         alt={recipe.title}/>
 
                         <div className="recipe__text">
-                            <h5 className="recipes__title"> { recipe.title }</h5>
+                            <h5 className="recipes__title"> { recipe.title.length < 20 ? `${recipe.title}` : 
+                            `${recipe.title.substring(0, 25)}...` } </h5>
                             <p  className="recipes__subtitles">Publisher: <span>
                                 { recipe.publisher }
                                 </span> </p>
