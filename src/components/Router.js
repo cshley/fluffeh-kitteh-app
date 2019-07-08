4ad6834be59d4af80004abb6a97348dc82c70090
Chloe Ashley <chloeashley193@gmail.com>
2019-07-08 17:15:35 +0100

    create Recipe component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "../App";
+import Recipe from './Recipe'
 
 const Router = () => (
 <BrowserRouter>
-<Route path="" component={} />
+<Route path="/" component={App} />
 </BrowserRouter>
 );
