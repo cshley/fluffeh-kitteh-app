6ea94ac971a93fbac55d8aada6a7cc556e9a4264
Chloe Ashley <chloeashley193@gmail.com>
2019-07-08 12:15:40 +0100

    form render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./app.css";
+import Form from './components/Form';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <header className="App-header">
        <h1 className="App-title">Recipe Search</h1>
+       <Form />
      </header>
     </div>
   );
