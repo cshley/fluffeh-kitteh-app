5a1ca8a24a697cf60953b299f9d8ff60193b68d3
Chloe Ashley <chloeashley193@gmail.com>
2019-07-09 10:14:26 +0100

    change of const names recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { async } from "q";
+
+const API_KEY = "d1f5a3acae70dc33db0f071926bd9bc9";
 
 class Recipe extends React.Component {
     state = {
         activeRecipe: []
     }
 
-    componentDidMount = () => {
-        
+    componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch
+    (`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+   
+    const res = await req.json();
+    console.log(res); 
+    
     }
 
     render() {
-        console.log(this.props);
         return (
             <div>recipe</div>
         );
